ebec233d7908a7671f397089c0cce9a37cedfb7c
wep1980 <wepbike@gmail.com>
2021-02-19 11:21:50 -0300

    Rest Template

---
+++ src/main/java/br/com/waldirdev/hrpayroll/config/AppConfig.java
@@ -0,0 +1,18 @@
+package br.com.waldirdev.hrpayroll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+
+@Configuration
+public class AppConfig {
+	
+	
+	
+	@Bean
+	public RestTemplate restTemplate () {
+		return new RestTemplate();
+	}
+
+}
